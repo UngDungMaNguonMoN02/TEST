6716a2cd65c7e40ba0cbcda3c0b168a8ad4b72ea
Toan <toanmanhbui@gmail.com>
2017-04-24 23:17:53 +0700

    plays a new audio file from a different folder in assets

--- app/src/main/java/com/example/toan/playaudiotest/MainActivity.java
+++ app/src/main/java/com/example/toan/playaudiotest/MainActivity.java
@@ -52,7 +52,8 @@ public class MainActivity extends AppCompatActivity
         mediaPlayer = new MediaPlayer();
         try  //Thay "audio.mp3" bằng đường dẫn khác
         {
-            AssetFileDescriptor afd = getAssets().openFd("audio.mp3");
+            //AssetFileDescriptor afd = getAssets().openFd("audio.mp3");
+            AssetFileDescriptor afd = getAssets().openFd("newfolder/audio2.mp3");
             mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
         }
         catch (Exception ex)
